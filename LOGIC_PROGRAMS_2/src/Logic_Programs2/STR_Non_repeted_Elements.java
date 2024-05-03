b59fc386d167e5ed95ef5288aba40487d09826ae
ANAZ <ANAZ@LAPTOP-EVTU5ED5>
2024-05-03 19:01:57 +0530

    Non Repeted Elementes program will be added

---
+++ LOGIC_PROGRAMS_2/src/Logic_Programs2/STR_Non_repeted_Elements.java
@@ -0,0 +1,21 @@
+package Logic_Programs2;
+
+public class STR_Non_repeted_Elements {
+
+	public static void main(String[] args) {
+		String str="AACCJAVA";
+		for(int i=0;i<str.length();i++) {
+			boolean unique=true;
+			for(int j=0;j<str.length();j++) {
+				char ch=str.charAt(j);
+				if(i!=j&& str.charAt(i)==str.charAt(j)) {
+					unique=false;
+				}
+			}
+			if(unique)
+				System.out.println(str.charAt(i));
+		}
+		
+	}
+
+}
